d97ea25da59bf90bcd8a5f1a91254588064a835a
Training4microservices <training4microservices@gmail.com>
2022-08-30 07:02:38 +0000

    Initial Commit

---
+++ src/test/java/com/training/spring/security/api/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.spring.security.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
